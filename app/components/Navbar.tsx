a7fbff82582228e8e55eba88a9d48d0994e5b371
s3xmaxman <badside3919@gmail.com>
2023-11-29 15:51:14 +0900

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+        <Link href="/">
+            <img src="" alt="" />
+        </Link>
+    </nav>
+  )
+}
+
+export default Navbar
